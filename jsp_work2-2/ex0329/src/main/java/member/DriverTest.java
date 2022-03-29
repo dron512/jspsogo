4051ca8d891b0e0940eaafec461cfa01c3dd36bb
dron512 <parkmyounghoi@gmail.com>
2022-03-29 16:01:08 +0900

    220329

--- jsp_work2-2/ex0329/src/main/java/member/DriverTest.java
+++ jsp_work2-2/ex0329/src/main/java/member/DriverTest.java
@@ -1,8 +1,48 @@
 package member;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DriverTest {
+
+	//select 하는 곳... 
+	public ArrayList<Member> doSelect(){
+		ArrayList<Member> al = new ArrayList<Member>();
+		
+		System.out.println("doSelect 왔음...");
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager
+					.getConnection(
+						"jdbc:mysql://localhost:3306/aaa", 
+						"root",
+						"1234");
+			pstmt = con
+					.prepareStatement(
+						"select * from member");
+			rs = pstmt.executeQuery();
+			while( rs.next() ) {
+				Member member = new Member();
+				member.setId(rs.getInt(1));
+				member.setUsername(rs.getString(2));
+				member.setPassword(rs.getString(3));
+				al.add(member);
+
+				System.out.print(rs.getInt(1));
+				System.out.print(rs.getString(2));
+				System.out.println(rs.getString(3));
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+//			return null;
+		}
+		return al;
+	}
+	
+	//insert 하는 곳...
 	public String doInsert(String username,String password) {
 		// 연결을 들고 있는거
 		Connection con;
--- jsp_work2-2/ex0329/src/main/webapp/index.jsp
+++ jsp_work2-2/ex0329/src/main/webapp/index.jsp
@@ -5,15 +5,14 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-<link href="https://fonts.googleapis.com/css?family=Black+Han+Sans:400" 
-		rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Black+Han+Sans:400" rel="stylesheet">
 <style>
 	*{
 		font-family: 'Black Han Sans', sans-serif;
 	}
 	.container{
 		margin: 0 auto;
-		width:600px;
+		width:700px;
 		background-color: #ccc;
 	}
 	ul{
@@ -21,21 +20,28 @@
 	}
 	li{
 		display : inline;
-		background-color: lime;
 	}
 	a{
+		background-color: lime;
 		display : inline-block;
 		margin : 1rem;
+		padding : 1rem;
 		text-decoration: none;
 	}
 </style>
 </head>
 <body>
 <!-- 1. tag태그명.. 2.id선택자 # 3.클래스선택자 . -->
+<!-- 
+	회원 crud
+	회원가입.. 회원로그인.. 회원삭제  
+	ppt 문서
+-->
+<!-- 게시판 crud ppt 문서-->
 <div class="container">
 	<ul>
 		<li><a href="ex01.jsp">1번 insert</a></li>
-		<li><a href="">2번 select</a></li>
+		<li><a href="select.jsp">2번 select</a></li>
 		<li><a href="">3번 delete</a></li>
 		<li><a href="">4번 update</a></li>
 	</ul>

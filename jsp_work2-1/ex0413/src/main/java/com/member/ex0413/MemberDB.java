91a7323a23d23ab1892526d686cb0a96fb577663
dron512 <parkmyounghoi@gmail.com>
2022-04-20 15:59:30 +0900

    220420

--- jsp_work2-1/ex0413/src/main/java/com/member/ex0413/MemberDB.java
+++ jsp_work2-1/ex0413/src/main/java/com/member/ex0413/MemberDB.java
@@ -10,8 +10,6 @@ import java.util.List;
 public class MemberDB {
     public List<Member> doselect(){
         List<Member> list = new ArrayList();
-//        list.add(new Member());
-
         Connection con = null;  // DB연결
         PreparedStatement pstmt = null; // SQL문 작성..
         ResultSet rs = null;  // DB Table 저장..
@@ -23,7 +21,6 @@ public class MemberDB {
             rs = pstmt.executeQuery();
             while (rs.next()){
                 Member member = new Member();
-
                 member.setId(rs.getInt("id"));
                 member.setUsername(rs.getString("username"));
                 member.setPassword(rs.getString("password"));
@@ -36,5 +33,32 @@ public class MemberDB {
             e.printStackTrace();
         }
         return list;
+    }
+    public void dodelete(String []ids){
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        // ids [ 2 3 4 ] -> 2,3,4
+        String values = "";
+        for ( int i =0; i<ids.length; i++){
+            if( i == (ids.length-1) )
+                values = values + ids[i];
+            else
+                values = values + ids[i]+",";
+        }
+        // 2,3,4,
+        System.out.println("values = "+values);
+        try{
+            // jar 로딩..
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // DB 연결
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/b-21",
+                    "root","1234");
+            // sql 구문 준비
+            pstmt = con.prepareStatement("delete from member where id in ("+values+")");
+            pstmt.executeUpdate();// sql 구문 실행...
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
